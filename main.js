d600005b882cde81433838a63dd73a1c4fe46aec
SanjanaRaj25 <sanjana.raj.25@dartmouth.edu>
2024-01-31 10:28:48 -0500

    styling

---
+++ main.js
@@ -0,0 +1,6 @@
+// // map arrows
+
+//     const startElement = document.getElementById('southfield');
+//     const endElement = document.getElementById('west-laf');
+
+//     new LeaderLine(startElement, endElement, {color: 'red', size: 8});
